410994c3fde0123b8ab08eed557eea0704550ce7
Tom Dolson <tomdolson@gmail.com>
2024-02-17 13:54:06 -0500

    Initial Commit

---
+++ PureandSoftWaterIpad/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PureandSoftWaterIpad
+//
+//  Created by Tom Dolson on 2/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PureandSoftWaterIpad/PureandSoftWaterIpadApp.swift
@@ -0,0 +1,17 @@
+//
+//  PureandSoftWaterIpadApp.swift
+//  PureandSoftWaterIpad
+//
+//  Created by Tom Dolson on 2/17/24.
+//
+
+import SwiftUI
+
+@main
+struct PureandSoftWaterIpadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
